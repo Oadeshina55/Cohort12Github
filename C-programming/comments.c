2c770631220df0cadaea5204b3d32360f9e1fac7
Oadeshina55 <123444511+Oadeshina55@users.noreply.github.com>
2023-03-23 22:29:44 +0000

    comments

---
+++ C-programming/comments.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* This function is used for a double line comment
+ * It is also not executable by the Editor
+ *
+ * Return 0
+ */
+int main()
+{
+	// This is a comment
+	printf("Line 5 is a comment");
+	return (0);
+}
---
+++ C-programming/new-lines.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("My First Program in C\nI am learning C.\nAnd it's Awesome");
+	return (0);
+}
