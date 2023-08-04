d9bb1cc2e62a9c42a7bc65deb13b627c3aacbbe0
Oadeshina55 <123444511+Oadeshina55@users.noreply.github.com>
2023-08-04 12:00:02 +0000

    odd_numbers.sh

--- BASH_LINUX/odd_numbers.sh
+++ BASH_LINUX/odd_numbers.sh
@@ -4,4 +4,4 @@ for ((i = 1; i <= 99; i += 2)); do
   echo $i
 done
 
-/*  here is a bash script that uses a for loop to display only the odd natural numbers from 1 to 99:
+/*  here is a bash script that uses a for loop to display only the odd natural numbers from 1 to 99
